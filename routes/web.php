87844db897b6189ad26a1fb9faa16fd12d2064e6
Rafael Santana de Brito <rafael.brito@atlastechnol.com>
2025-01-12 10:37:10 -0300

    chore: adjustment

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::prefix('orcamento')->group(function () {
-        Route::get('/', MontarOrcamentoController::class)->name('orcamento.montar');
-        Route::post('/', GerarOrcamentoController::class)->name('orcamento.gerar');
-    });
 });
 
+Route::prefix('orcamento')->group(function () {
+    Route::get('/', MontarOrcamentoController::class)->name('orcamento.montar');
+    Route::post('/', GerarOrcamentoController::class)->name('orcamento.gerar');
+});
 
 require __DIR__.'/auth.php';
